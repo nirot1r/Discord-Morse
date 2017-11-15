ab9b6a917f9c3e7cd1ac29e3a18438ee82705b54
小野咲みなみ <mnmonzk@gmail.com>
2017-11-15 23:32:27 +0900

    ✏ モールス信号リストの代入

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 /* 初期化処理 */
-const Discord = require("discord.js");
-const Token = require("./config/Token.js")
-const Client = new Discord.Client();
+    const FS = require("fs");
+    const Discord = require("discord.js");
+    const Token = require("./config/Token.js")
+    const Client = new Discord.Client();
 
+/* モールス信号リスト代入 */
+    const morseEnglish = JSON.parse(FS.readFileSync("./assets/english.json"), "utf8");
+    const morseJapanese = JSON.parse(FS.readFileSync("./assets/japanese.json"), "utf8");
+
+    console.log(morseEnglish);
+    console.log(morseJapanese);
+    
 /* ログイン処理 */
     Client.login(Token.discord);
 
@@ -12,7 +20,9 @@ const Client = new Discord.Client();
     });
   
     Client.on("message", message => {
-        if (message.content === "関口!") {
+        if (message.content === "関口!" || message.content === "キャプテン!") {
             message.reply("翼!");
+        } else if(message.content === "益若!") {
+            message.reply("つばさ!");
         }
     });
