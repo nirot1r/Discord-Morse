b6fd985dba6e3ef17fd2b5b14d7929d01d71a542
小野咲みなみ <mnmonzk@gmail.com>
2017-11-16 13:20:32 +0900

    ✏ モールス変換の限定化 (🔧 #3)

--- app.js
+++ app.js
@@ -9,6 +9,7 @@
     const morseJapanese = JSON.parse(FS.readFileSync("./assets/japanese.json"), "utf8");
     const morseNumber = JSON.parse(FS.readFileSync("./assets/number.json"), "utf8");
 
+
 /* 関数定義 */
     const h2k = function(content) {
         return content.replace(/[\u3041-\u3096]/g, function(match) {
@@ -16,6 +17,9 @@
             return String.fromCharCode(chr);
         });
     }
+
+/* 変数定義 */
+    const reactionCommand = /^M\s(.+)$/i;
 
 /* ログイン処理 */
     Client.login(Token.discord);
@@ -27,42 +31,45 @@
   
     Client.on("message", message => {
         if(message.member.id != 380359638996090880) {
-            let processMessage; // 処理を行うメッセージ
-            let returnMessage; // 返すメッセージ
+            if(reactionCommand.test(message.content)) {
+                const rawMessage = message.content.match(reactionCommand)[1]; // 処理を行うメッセージ
+                let processMessage; // 処理の一時使用変数
+                let returnMessage; // 返すメッセージ
 
-            /* 英語の置き換え処理 */ // リファクタリング必須
-                processMessage = message.content.toUpperCase();
-                returnMessage = "";
-                Object.keys(morseEnglish).forEach(function(key) {
-                    if(returnMessage === "") {
-                        returnMessage = processMessage.split(key).join(morseEnglish[key] + "　");
-                    } else {
-                        returnMessage = returnMessage.split(key).join(morseEnglish[key] + "　");
-                    }
-                });
-            
-            /* 日本語の置き換え処理 */ // リファクタリング必須
-                processMessage = h2k(returnMessage);
-                returnMessage = "";
-                Object.keys(morseJapanese).forEach(function(key) {
-                    if(returnMessage === "") {
-                        returnMessage = processMessage.split(key).join(morseJapanese[key] + "　");
-                    } else {
-                        returnMessage = returnMessage.split(key).join(morseJapanese[key] + "　");
-                    }
-                });
+                /* 英語の置き換え処理 */ // リファクタリング必須
+                    processMessage = rawMessage.toUpperCase();
+                    returnMessage = "";
+                    Object.keys(morseEnglish).forEach(function(key) {
+                        if(returnMessage === "") {
+                            returnMessage = processMessage.split(key).join(morseEnglish[key] + "　");
+                        } else {
+                            returnMessage = returnMessage.split(key).join(morseEnglish[key] + "　");
+                        }
+                    });
+                
+                /* 日本語の置き換え処理 */ // リファクタリング必須
+                    processMessage = h2k(returnMessage);
+                    returnMessage = "";
+                    Object.keys(morseJapanese).forEach(function(key) {
+                        if(returnMessage === "") {
+                            returnMessage = processMessage.split(key).join(morseJapanese[key] + "　");
+                        } else {
+                            returnMessage = returnMessage.split(key).join(morseJapanese[key] + "　");
+                        }
+                    });
 
-            /* 数字の置き換え処理 */ // リファクタリング必須
-                processMessage = returnMessage;
-                returnMessage = "";
-                Object.keys(morseNumber).forEach(function(key) {
-                    if(returnMessage === "") {
-                        returnMessage = processMessage.split(key).join(morseNumber[key] + "　");
-                    } else {
-                        returnMessage = returnMessage.split(key).join(morseNumber[key] + "　");
-                    }
-                });
+                /* 数字の置き換え処理 */ // リファクタリング必須
+                    processMessage = returnMessage;
+                    returnMessage = "";
+                    Object.keys(morseNumber).forEach(function(key) {
+                        if(returnMessage === "") {
+                            returnMessage = processMessage.split(key).join(morseNumber[key] + "　");
+                        } else {
+                            returnMessage = returnMessage.split(key).join(morseNumber[key] + "　");
+                        }
+                    });
 
-            message.channel.send("`" + returnMessage + "`");
+                message.channel.send("`" + returnMessage + "`");
+            }
         }
     });
